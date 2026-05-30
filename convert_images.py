db20554c3ed53f8aadddffa16d67c2b6cbfc91c7
Marsha Shearin <marshakprogrammer@gmail.com>
2026-05-30 13:09:37 -0400

    converted images to webp format for faster loading

---
+++ convert_images.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+images = [
+    'static/images/Delegate.png',
+    'static/images/EH.png',
+    'static/images/Lux.png',
+    'static/images/MarshaK.png'
+]
+
+for path in images:
+    img = Image.open(path)
+    webp_path = path.replace('.png', '.webp')
+    img.save(webp_path, 'WEBP', quality=80, optimize=True)
+    orig = os.path.getsize(path)//1024
+    new = os.path.getsize(webp_path)//1024
+    print(f'{path}: {orig}KB -> {new}KB')
